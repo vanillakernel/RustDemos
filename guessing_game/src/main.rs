49171b0d4476f95011aff26d2d7e6b8f50f3e96c
vanillakernel <vanillakernel@gmail.com>
2015-12-12 06:51:06 -0800

    more stuff

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,20 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+
+println!("Guess the number!");
+
+println!("Please input your guess.");
+
+//create a new mutable string object
+let mut guess = String::new();
+
+// update guess string with input from terminal
+// if nothing, throw the error.
+io::stdin().read_line(&mut guess)
+   .expect("Failed to read line.");
+
+// {} is a placeholder for the variable?
+println!("You guessed: {}", guess);
+
 }
