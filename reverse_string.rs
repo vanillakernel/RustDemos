8a385e67c285a815a75e7f2c0d9b7f74f6b9690f
vanillakernel <vanillakernel@gmail.com>
2015-12-15 16:17:37 -0800

    rust is fussy my god

--- reverse_string.rs
+++ reverse_string.rs
@@ -2,6 +2,38 @@ use std::io;
 
 fn main() {
 
-println!("Input a string and I will reverse it!");
+ println!("Input a string and I will reverse it!");
 
+ println!("Dat string:");
+
+ let mut guess = String::new();
+
+ io::stdin().read_line(&mut guess)
+    .expect("Failed to read line.");
+ //Trim that string
+ let trimmed = &guess.trim();
+ for c in trimmed.chars() {
+        print!("{},  ", c);
+ }
+
+ //Return each character
+ let mut range = 0..trimmed.len();
+ let mut character_vec = Vec::new();
+ let characters = trimmed.chars();
+ //let reversed = characters.reverse();
+ 
+
+ loop {
+  match range.next() {
+    Some(x) => {
+      let character = trimmed.chars().nth(x);  
+      //println!("{:?}", character);
+      character_vec.push(character);
+    }
+    None => {
+    break 
+    }    
+  println!("{:?}", character_vec.reverse());
+  }
+ } 
 }
